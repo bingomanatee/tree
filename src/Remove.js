441f390d39b7f82bc3453b3064725e375789ea5b
David delhart <dedelhart@voicebase.com>
2020-01-28 12:20:50 -0800

    adding remove

---
+++ src/Remove.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function SvgRemove(props) {
+  return (
+    <svg width="21px" height="20px" viewBox="0 0 21 20" {...props}>
+      <title>{"remove"}</title>
+      <desc>{"Created with Sketch."}</desc>
+      <g
+        id="remove"
+        stroke="none"
+        strokeWidth={1}
+        fill="none"
+        fillRule="evenodd"
+      >
+        <circle id="Oval" stroke="#D0021B" cx={11} cy={10} r={7} />
+        <rect id="Rectangle" fill="#D0021B" x={6} y={9} width={10} height={2} />
+      </g>
+    </svg>
+  );
+}
+
+export default SvgRemove;
--- src/Tree.js
+++ src/Tree.js
@@ -6,9 +6,11 @@ import Checkbox from './Checkbox'
 import Empty from './Empty'
 import Mutable from './Mutable'
 import Add from './Add'
+import Remove from './Remove';
 
 const Tree = memo(({ children, name, style, defaultOpen = false }) => {
   const [isOpen, setOpen] = useState(defaultOpen)
+  const [removed, setRemoved] = useState(false)
   const [extra, setExtra] = useState([])
   const previous = usePrevious(isOpen)
   const [bind, { height: viewHeight }] = useMeasure()
@@ -16,8 +18,8 @@ const Tree = memo(({ children, name, style, defaultOpen = false }) => {
     from: { height: 0, opacity: 0, transform: 'translate3d(20px,0,0)' },
     to: { height: isOpen ? viewHeight : 0, opacity: isOpen ? 1 : 0, transform: `translate3d(${isOpen ? 0 : 20}px,0,0)` }
   })
-  console.log('rendering with extra', extra)
 
+  if (removed) return '';
   return (
     <Frame>
       <Center>
@@ -25,6 +27,7 @@ const Tree = memo(({ children, name, style, defaultOpen = false }) => {
           setOpen(e.target.value)
         }}/> : <Empty/>}</CheckboxWrapper>
         <Title style={style}>{name}</Title>
+        <Remove onClick={() => setRemoved(true)}></Remove>
       </Center>
       <Content style={{ opacity, height: isOpen && previous === isOpen ? 'auto' : height }}>
         <a.div style={{ transform }} {...bind}>
@@ -33,13 +36,16 @@ const Tree = memo(({ children, name, style, defaultOpen = false }) => {
       </Content>
       {extra.map(e => (
         <Center>
-          <CheckboxWrapper><Empty/></CheckboxWrapper> {e}
+          <CheckboxWrapper><Empty/></CheckboxWrapper>
+          {e}
+          <Remove onClick={() => setExtra(extra.filter(a => a !== e))}></Remove>
         </Center>
       ))}
       <Center onClick={() => {
         setExtra([...extra, <Mutable/>])
       }}>
-        <CheckboxWrapper noIndent><Add/></CheckboxWrapper><Title style={{ color: '#999999' }}>Add Item</Title>
+        <CheckboxWrapper noIndent><Add/></CheckboxWrapper>
+        <Title style={{ color: '#999999' }}>Add Item</Title>
       </Center>
     </Frame>
   )
