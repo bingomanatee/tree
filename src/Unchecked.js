41d7226334e4b199589eabbf0ec8d5fc2ae02c37
Dave Edelhart <dave@wonderlandlabs.com>
2020-01-28 10:11:54 -0800

    Initial commit

---
+++ src/Unchecked.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Unchecked(props) {
+  return (
+    <svg width="31px" height="29px" viewBox="0 0 31 29" {...props}>
+      <title>{'unchecked'}</title>
+      <desc>{'Created with Sketch.'}</desc>
+      <g id="unchecked" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+        <rect id="Rectangle-Copy" fillOpacity={0.502868226} fill="#D8D8D8" x={5} y={4} width={26} height={25} />
+        <rect id="Rectangle" stroke="#FFFFFF" fill="#4A4A4A" x={1} y={1} width={25} height={25} />
+      </g>
+    </svg>
+  )
+}
+
+export default Unchecked
